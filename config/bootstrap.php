fcdc94d8cd00770eb4ced612eb1f6b52034226b4
AndreyDuda <andreyduda88@gmail.com>
2019-04-23 22:25:03 +0300

    configuration doctrine

---
+++ config/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require "vendor/autoload.php";
+$isDevMode = true;
+
+$config = Setup::createAnnotationMetadataConfiguration(array("./src"), $isDevMode);
+
+$connection = array(
+    'dbname'   => 'doctrine',
+    'user'     => 'doctrine',
+    'password' => 'doctrine',
+    'host'     => 'mysql',
+    'driver'   => 'pdo_mysql'
+);
+
+$entityManager = EntityManager::create($connection, $config);
---
+++ config/cli-config.php
@@ -0,0 +1,3 @@
+<?php
+require "bootstrap.php";
+return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
