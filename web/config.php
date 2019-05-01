f5e3392f121f6de1bfd1e29b2c8b5856402129df
AndreyDuda <AndreyDuda88@gmail.com>
2019-05-01 18:06:47 +0300

    add symfony config completed

--- web/config.php
+++ web/config.php
@@ -17,6 +17,7 @@ if (!isset($_SERVER['HTTP_HOST'])) {
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
+    $_SERVER['REMOTE_ADDR']
 ))) {
     header('HTTP/1.0 403 Forbidden');
     exit('This script is only accessible from localhost.');
